7b0746860576874e66a82f09666598fd37e77287
Jeffeson-Queiroz <jeffqueiroz1@hotmail.com>
2026-01-07 16:30:59 -0300

    Configura CORS para frontend local e Netlify

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,15 @@ const cors = require("cors");
 const pool = require("./db");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://catalogo-filmeprojweb2.netlify.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"]
+}));
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
